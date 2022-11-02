bb5f93514412c613d0297e427536792d88425f85
JR DevPM <astronomi@gmail.com>
2022-11-02 14:23:48 -0300

    Update with useFonts and StatusBar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,19 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { NativeBaseProvider, Center, Text } from "native-base";
+import { Loading } from './src/components/Loading'
+import { SignIn } from './src/screens/SignIn'
 
 import { THEME } from './src/styles/theme'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <Center flex={1} bgColor="gray.900">
-        <Text color="white" fontSize={24}>
-          Hello World!!!
-        </Text>
-        <StatusBar style="auto" />
-      </Center>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      { fontsLoaded ? <SignIn /> : <Loading /> }
     </NativeBaseProvider>
   );
 }
