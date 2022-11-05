a64ab20cbb3960c0f8ed89656c792517c6aa2447
JR DevPM <astronomi@gmail.com>
2022-11-05 11:34:17 -0300

    Create new screen

---
+++ mobile/src/screens/New.tsx
@@ -0,0 +1,11 @@
+import { VStack, Text } from 'native-base'
+
+export function New() {
+    return (
+        <VStack flex={1} bg="gray.900" p={7}>
+           <Text>
+            New
+           </Text>
+        </VStack>
+    )
+}
