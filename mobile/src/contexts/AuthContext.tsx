995acf66ac1ee385edfc4aab9d9bf964de417e20
JR DevPM <astronomi@gmail.com>
2022-11-05 11:42:36 -0300

    Update with AuthSession and WebBrowser

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,9 @@
-import { createContext, ReactNode } from "react"
+import { createContext, ReactNode, useEffect, useState } from "react"
+import * as Google from "expo-auth-session/providers/google"
+import * as AuthSession from "expo-auth-session"
+import * as WebBrowser from "expo-web-browser"
+
+WebBrowser.maybeCompleteAuthSession()
 
 interface UserProps {
   name: string;
@@ -11,25 +16,50 @@ interface AuthProviderProps {
 
 export interface AuthContextDataProps {
   user: UserProps;
+  isUserLoading: boolean;
   signIn: () => Promise<void>
 }
 
 export const AuthContext = createContext({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
+  const [user, seuUser] = useState<UserProps>({} as UserProps)
+  const [isUserLoading, setIsUserLoading] = useState(false)
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: 'seu numero da google aqui',
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ['profile', 'email']
+  })
 
   async function signIn() {
-    console.log("ensta no signin!!!");
-    
+    try {
+      setIsUserLoading(true)
+      
+      await promptAsync()
+    } catch (error) {
+      console.log(error)
+      throw error
+    } finally {
+      setIsUserLoading(false)
+    }
   }
+
+  async function signInWithGoogle(access_token: string) {
+    console.log('== Token de Autenticate ===>', access_token);
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success' && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken)
+    }
+  }, [response])
 
   return (
     <AuthContext.Provider value={{
       signIn,
-      user: {
-        name: "Humberto Jr",
-        avatarUrl: "https://github.com/humbertoromanojr.png",
-      }
+      isUserLoading,
+      user,
     }}>
       {children}
     </AuthContext.Provider>
