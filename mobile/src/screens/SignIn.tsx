caa38459a5668c40acf6b6e78f879ee1b2f2910b
JR DevPM <astronomi@gmail.com>
2022-11-04 22:00:22 -0300

    Show the other attributes of the button

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,11 +1,20 @@
-import { Center, Text } from 'native-base'
+import { Center, Text, Icon } from 'native-base'
+import { Fontisto } from '@expo/vector-icons'
+
+import { Button } from '../components/Button'
+import LogoCup2022 from '../assets/logo.svg'
 
 export function SignIn() {
   return (
     <Center flex={1} bg="gray.900">
+      <LogoCup2022 width={262} height={90} />
       <Text color="white" fontSize={24} fontFamily="heading">
         SignIn
       </Text>
+      <Button 
+        title="Entrar com Google" 
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />} 
+      />
     </Center>
   )
 }
