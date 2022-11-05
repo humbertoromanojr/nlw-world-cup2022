cb2ae11d32532214a1297f55399a41f919f300a1
JR DevPM <astronomi@gmail.com>
2022-11-04 23:50:25 -0300

    Finished button component

--- mobile/src/components/Button.tsx
+++ mobile/src/components/Button.tsx
@@ -2,12 +2,32 @@ import { Button as ButtonNativeBase, Text, IButtonProps } from "native-base"
 
 interface Props extends IButtonProps {
   title: string;
+  type?: 'PRIMARY' | 'SECONDARY';
 }
 
-export function Button({ title, ...rest }: Props) {
+export function Button({ title, type = 'PRIMARY', ...rest }: Props) {
   return (
-    <ButtonNativeBase {...rest}>
-      <Text>{title}</Text>
+    <ButtonNativeBase
+      w="full"
+      h={14}
+      rounded="sm"
+      fontSize="md"
+      bg={type === 'SECONDARY' ? 'red.500' : 'yellow.500'}
+      _pressed={{
+        bg: type === 'SECONDARY' ? 'red.600' : 'yellow.600'
+      }}
+
+      {...rest}
+    >
+      <Text 
+        paddingLeft={2} 
+        textTransform="uppercase"
+        fontFamily="heading"
+        fontSize="sm"
+        color={type === 'SECONDARY' ? 'white' : 'black'}
+      >
+        {title}
+      </Text>
     </ButtonNativeBase>
   )
 }
