eadd099292a7467bd47d91c25e3e8affde11bc70
JR DevPM <astronomi@gmail.com>
2022-11-06 19:41:07 -0300

    Reloading the list correctly

--- mobile/src/screens/Polls.tsx
+++ mobile/src/screens/Polls.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react"
+import { useState, useCallback } from "react"
 import { VStack, Icon, useToast, FlatList } from "native-base"
 import { Fontisto } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native'
+import { useFocusEffect } from '@react-navigation/native'
 
 import { api } from '../services/api'
 
@@ -44,9 +45,9 @@ export function Pools() {
 
   }
 
-  useEffect(() => {
+  useFocusEffect(useCallback(() => {
     fetchPools()
-  }, [])
+  }, []))
 
     return (
         <VStack flex={1} bgColor="gray.900">
